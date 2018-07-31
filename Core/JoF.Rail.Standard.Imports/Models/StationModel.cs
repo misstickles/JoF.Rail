1bc582bb673333935dce0ab6d3f67c9ae5754988
Jo Faircloth <jo.faircloth@ihsmarkit.com>
2018-07-31 16:28:31 +0100

    started...

---
+++ Core/JoF.Rail.Standard.Imports/Models/StationModel.cs
@@ -0,0 +1,44 @@
+﻿namespace JoF.Rail.Standard.Imports.Models
+{
+    using System.Collections.Generic;
+    using System.Xml;
+    using System.Xml.Serialization;
+
+    /// <summary>
+    /// A class for the station data the we actually need
+    /// </summary>
+    [XmlRoot("Station")]
+    public class StationModel
+    {
+        [XmlElement("CrsCode")]
+        public string CrsCode { get; set; }
+
+        [XmlElement("AlternativeIdentifiers/NationalLocationCode")]
+        public string NationalLocationCode { get; set; }
+
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("SixteenCharacterName")]
+        public string SixteenCharacterName { get; set; }
+
+        [XmlElement("Latitude")]
+        public double Latitude { get; set; }
+
+        [XmlElement("Longitude")]
+        public double Longitude { get; set; }
+
+        [XmlElement("StationOperator")]
+        public string StationOperator { get; set; }
+
+        [XmlElement("TrainOperatingCompanies")]
+        public List<Toc> Tocs { get; set; }
+
+        [XmlType("TrainOperatingCompanies")]
+        public class Toc
+        {
+            [XmlElement("TocRef")]
+            public string Ref { get; set; }
+        }
+    }
+}
