dfdff4c4971fbe5bf3358dcf8ec93756678e0405
misstickles <jofaircloth@gmail.com>
2018-08-08 23:55:29 +0100

    Changes to enable gulp build on Azure

--- JoF.Rail.Core.Web/gulpfile.js
+++ JoF.Rail.Core.Web/gulpfile.js
@@ -7,31 +7,55 @@ var gulp = require("gulp"),
     rename = require("gulp-rename"),
     cleanCss = require("gulp-clean-css");
 
+gulp.task("fa-fonts", function () {
+    return gulp.src([
+        "wwwroot/lib/Font-Awesome/web-fonts-with-css/webfonts/fa-solid-*.*"])
+        .pipe(gulp.dest("wwwroot/build/webfonts"))
+        .pipe(gulp.dest("wwwroot/dist/webfonts"));
+});
 
 gulp.task("sass", function () {
     return gulp.src("wwwroot/scss/site.scss")
         .pipe(sass())
         .pipe(rename("site_scss.css"))
-        .pipe(gulp.dest('wwwroot/dist/css'));
+        .pipe(gulp.dest('wwwroot/build/css'));
 });
 
-gulp.task("minify-css", ["sass"], () => {
-    gulp.src([
-        "wwwroot/lib/jquery-ui/themes/base/jquery-ui.css",
+gulp.task("copy-css", ["sass"], function () {
+    return gulp.src([
+        "wwwroot/lib/Font-Awesome/web-fonts-with-css/css/fontawesome-all.css",
         "wwwroot/css/site.css",
-        "wwwroot/dist/css/site_scss.css"])
+        "wwwroot/build/css/site_scss.css"])
+        .pipe(gulp.dest("wwwroot/build/css"));
+});
+
+gulp.task("concat-css", ["copy-css"], function () {
+    return gulp.src([
+        "wwwroot/build/site.css",
+        "wwwroot/build/site_scss.css"])
         .pipe(concat("combined.css"))
-        .pipe(gulp.dest("wwwroot/dist/css"))
-        .pipe(cleanCss({ debug: true }, (details) => {
-            console.log(`${details.name}: ${details.stats.originalSize}`);
-            console.log(`${details.name}: ${details.stats.minifiedSize}`);
-        }))
+        .pipe(gulp.dest("wwwroot/build/css"))
+});
+
+gulp.task("minify-css", ["concat-css"], function () {
+    return gulp.src([
+        "wwwroot/build/css/fontawesome-all.css",
+        "wwwroot/build/css/combined.css"])
+        .pipe(cleanCss())
         .pipe(rename({ suffix: ".min" }))
         .pipe(gulp.dest("wwwroot/dist/css"));
 });
 
-gulp.task("minify-js", function () {
-    gulp.src("wwwroot/js/site.js")
+gulp.task("copy-js", function () {
+    return gulp.src([
+        "wwwroot/lib/jquery/dist/jquery.min.js",
+        "wwwroot/lib/jquery-ui/dist/jquery-ui.min.js",
+        "wwwroot/lib/bootstrap/dist/js/bootstrap.min.js"])
+        .pipe(gulp.dest("wwwroot/dist/js"));
+});
+
+gulp.task("minify-js", ["copy-js"], function () {
+    return gulp.src("wwwroot/js/site.js")
         .pipe(uglify())
         .pipe(rename({ suffix: ".min" }))
         .pipe(gulp.dest("wwwroot/dist/js"));
@@ -41,11 +65,4 @@ gulp.task("watch", function () {
     gulp.watch("wwwroot/scss/*.scss", ["minify-css"]);
 });
 
-//gulp.task("bootstrap", function () {
-//    return gulp.src("wwwroot/lib/bootstrap/scss/*.scss")
-//        .pipe(sass())
-//        .pipe(concatCss("wwwroot/scss/bootstrap.css"))
-//        .pipe(gulp.dest('wwwroot/scss'))
-//});
-
-gulp.task("default", ["minify-js", "minify-css"], function (callback) { });
+gulp.task("default", ["minify-js", "minify-css", "fa-fonts"], function (callback) { });
